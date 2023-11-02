818b217c6835bfea1652cb9add3c7bc04e5a8c60
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-03 01:04:23 +0330

    add

--- src/App.js
+++ src/App.js
@@ -4,33 +4,41 @@ import logo from "./logo.svg";
 import "./App.css";
 import { render } from "@testing-library/react";
 
-class App extends Component {
+class App extends Component { 
   constructor() {
     super();
 
     this.state = {
-      monster: [
-        {
-          name: "hanie",
-        },
-        {
-          name: "heili",
-        },
-        {
-          name: "eminem",
-        },
-      ],
+      monster: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monster: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
   }
 
   render() {
     return (
       <div className="App">
-       {
-       this.state.monster.map((monster) => {
-        return <h1>{monster.name}</h1>
-       })
-       }
+        <input className="search-box" type="search" placeholder="search monsters" />
+        {this.state.monster.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
